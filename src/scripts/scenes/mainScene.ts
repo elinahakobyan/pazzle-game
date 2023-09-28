65775a72a393c0450fc17bf2b0b1992e0601760a
elina <elinahakobyan77@gmail.com>
2023-09-28 15:42:15 +0400

    setup project

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -1,6 +1,3 @@
-import PhaserLogo from '../objects/phaserLogo'
-import FpsText from '../objects/fpsText'
-
 export default class MainScene extends Phaser.Scene {
   fpsText
 
@@ -9,19 +6,18 @@ export default class MainScene extends Phaser.Scene {
   }
 
   create() {
-    new PhaserLogo(this, this.cameras.main.width / 2, 0)
-    this.fpsText = new FpsText(this)
-
+    // new PhaserLogo(this, this.cameras.main.width / 2, 0)
+    // this.fpsText = new FpsText(this)
     // display the Phaser.VERSION
-    this.add
-      .text(this.cameras.main.width - 15, 15, `Phaser v${Phaser.VERSION}`, {
-        color: '#000000',
-        fontSize: '24px'
-      })
-      .setOrigin(1, 0)
+    // this.add
+    //   .text(this.cameras.main.width - 15, 15, `Phaser v${Phaser.VERSION}`, {
+    //     color: '#000000',
+    //     fontSize: '24px'
+    //   })
+    //   .setOrigin(1, 0)
   }
 
   update() {
-    this.fpsText.update()
+    // this.fpsText.update()
   }
 }
