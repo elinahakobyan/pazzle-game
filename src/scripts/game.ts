c4e3fd5b46537d041bfdb6e75b02ccf6c4c0f95b
Elina-dc <elina.hakobyan@doublecoconut.com>
2023-09-29 18:48:47 +0400

    added mask

--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -2,18 +2,17 @@ import 'phaser'
 import MainScene from './scenes/mainScene'
 import PreloadScene from './scenes/preloadScene'
 
-const DEFAULT_WIDTH = 1280
-const DEFAULT_HEIGHT = 720
+
 
 const config = {
   type: Phaser.AUTO,
   backgroundColor: '#ffffff',
+  width: 1024,
+  height: 768,
   scale: {
-    parent: 'phaser-game',
-    mode: Phaser.Scale.FIT,
+    parent: "phaser-engine",
     autoCenter: Phaser.Scale.CENTER_BOTH,
-    width: DEFAULT_WIDTH,
-    height: DEFAULT_HEIGHT
+    mode: Phaser.Scale.ScaleModes.RESIZE ,
   },
   scene: [PreloadScene, MainScene],
   physics: {
--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -1,3 +1,5 @@
+import Sprite = Phaser.GameObjects.Sprite;
+
 export default class MainScene extends Phaser.Scene {
   fpsText
 
@@ -6,6 +8,30 @@ export default class MainScene extends Phaser.Scene {
   }
 
   create() {
+    console.log('Start')
+    const row=2
+    const col=2
+
+
+    const sprite=new Sprite(this,window.innerWidth*0.5,window.innerHeight*0.5,'phaser-logo')
+    this.add.existing(sprite)
+
+    const gr=this.add.graphics()
+    gr.fillRect(0,0,sprite.width/row,sprite.height/col)
+    gr.fillStyle(0x000fff,0.5)
+
+    gr.setPosition(sprite.x-sprite.width/2,sprite.y-sprite.height/2)
+
+    const mask = gr.createGeometryMask();
+   const a= sprite.setMask(mask);
+    console.log(a)
+   // a.setPosition(200,100)
+   // gr.setPosition(a.x,a.y)
+
+    console.log(sprite.width,sprite.height)
+
+
+
     // new PhaserLogo(this, this.cameras.main.width / 2, 0)
     // this.fpsText = new FpsText(this)
     // display the Phaser.VERSION
